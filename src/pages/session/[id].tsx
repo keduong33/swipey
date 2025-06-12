1aab3647a30c1bcff7c212f3ac0d1f19890eb215
Ke Duong <kelyduong@gmail.com>
2025-06-12 23:38:12 +1000

    Set up basic Join Session and Session page

---
+++ src/pages/session/[id].tsx
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+
+export default function Session() {
+    const router = useRouter();
+    const id = router.query.id
+
+    return <>ID: {id}</>
+}
--- src/pages/session/index.tsx
+++ src/pages/session/index.tsx
@@ -1,4 +1,45 @@
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import Router from "next/router"
+import { FormEvent } from "react"
+
+const formSessionCode = 'session-code'
+
+function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const sessionCode = formData.get(formSessionCode) as string
+    Router.push(`/session/${sessionCode}`)
+}
 
 export default function JoinSession() {
-    return <>Join Session</>
+    return (
+        <form onSubmit={onSubmit}>
+            <section className="flex w-full min-h-screen items-center justify-center">
+                <Card className="w-full max-w-sm m-4">
+                    <CardHeader className="justify-center">
+                        <CardTitle>Enter session code</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <Input placeholder="Session Code" className="text-center" name={formSessionCode}></Input>
+
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Join
+                        </Button>
+                    </CardFooter>
+                </Card>
+
+            </section >
+        </form>
+    )
 }
