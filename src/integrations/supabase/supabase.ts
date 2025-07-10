23b586355ee00d5469d2917c78264b6dc43c03e0
Ke Duong <kelyduong@gmail.com>
2025-07-10 21:39:24 +1000

    Add supabase integration

--- src/integrations/supabase/supabase.ts
+++ src/integrations/supabase/supabase.ts
@@ -1,8 +1,9 @@
 import { createServerClient } from '@supabase/ssr';
 import { parseCookies, setCookie } from '@tanstack/react-start/server';
+import { Database } from './database.types';
 
 export function getSupabaseServerClient() {
-    return createServerClient(
+    return createServerClient<Database>(
         process.env.SUPABASE_URL!,
         process.env.SUPABASE_ANON_KEY!,
         {
